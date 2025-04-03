c80187dfada58a6c9cf8b928b4943dc2b8c5be51
Ameer Hamza <ameerhamzagcu1060@gmail.com>
2025-04-03 18:05:09 +0500

    changed url field to image field

--- customer/urls.py
+++ customer/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
-router.register(r'create', CustomerViewSet, basename='customer')
+router.register(r'customer/create', CustomerViewSet, basename='customer')
 
 urlpatterns = [
     path('', include(router.urls)),
